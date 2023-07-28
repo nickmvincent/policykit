440a0f516f4065083181688d7f65cf1e75e13793
nickmvincent <nickmvincent@gmail.com>
2023-07-28 03:17:32 +0000

    fill in procedures_code for 4 CV voting choice

--- policykit/policytemplates/procedures_code.py
+++ policykit/policytemplates/procedures_code.py
@@ -33,31 +33,29 @@ def peer_approval__check():
 
     yes_votes = proposal.get_yes_votes().count()
     no_votes = proposal.get_no_votes().count()
-    proposal.data.set("yes_votes_num", yes_votes)
-    proposal.data.set("no_votes_num", no_votes)
-    logger.debug(f"{yes_votes} for, {no_votes} against")
+    logger.debug(f'{yes_votes} for, {no_votes} against')
 
     now = datetime.datetime.now().timestamp()
     reminder_delta = now - float(proposal.data.get('reminder_sent'))
 
-    if datetime.timedelta(seconds=int(reminder_delta)) > datetime.timedelta(minutes=5):
+    if datetime.timedelta(seconds=int(reminder_delta)) > datetime.timedelta(days=variables.reminder_window_in_days):
         proposal.data.set('reminder_sent', now)
 
-    already_voted = proposal.get_all_boolean_votes().values_list("user__username")
-    already_voted_list = [voter[0] for voter in already_voted]
-    nonvoter_list = [f"<@{uid}>" for uid in variables.users if uid not in already_voted_list]
-    nonvoter_string = ", ".join(nonvoter_list)
-    voter_msg = f"Eligible voters: {variables.users}; Already voted: {already_voted}; Nonvoter list: {nonvoter_list}"
-    slack.post_message(text=voter_msg, channel=variables.vote_channel, thread_ts=proposal.vote_post_id)
+        already_voted = proposal.get_all_boolean_votes().values_list('user__username')
+        already_voted_list = [voter[0] for voter in already_voted]
+        nonvoter_list = [f'<@{uid}>' for uid in variables.users if uid not in already_voted_list]
+        nonvoter_string = ', '.join(nonvoter_list)
+        voter_msg = f'Eligible voters: {variables.users}; Already voted: {already_voted}; Nonvoter list: {nonvoter_list}'
+        slack.post_message(text=voter_msg, channel=variables.vote_channel, thread_ts=proposal.vote_post_id)
 
-    reminder_msg = f"Please vote on this proposal. {nonvoter_string} have not voted yet."
-    slack.post_message(text=reminder_msg, channel=variables.vote_channel, thread_ts=proposal.vote_post_id)
+        reminder_msg = f'Please vote on this proposal. {nonvoter_string} have not voted yet.'
+        slack.post_message(text=reminder_msg, channel=variables.vote_channel, thread_ts=proposal.vote_post_id)
 
-    slack.post_message(text="Ran a check", channel=variables.vote_channel, thread_ts=proposal.vote_post_id)
+    # slack.post_message(text='Ran a check', channel=variables.vote_channel, thread_ts=proposal.vote_post_id)
 
     if yes_votes >= 1:
         return PASSED
-    elif no_votes >= variables.num_no_votes_to_reject:
+    elif no_votes >= variables.no_votes_to_fail:
         return FAILED
 
     return PROPOSED
@@ -68,9 +66,24 @@ def majority_vote__check():
         return None
     yes_votes = proposal.get_yes_votes().count()
     no_votes = proposal.get_no_votes().count()
-    proposal.data.set('yes_votes_num', yes_votes)
-    proposal.data.set('no_votes_num', no_votes)
     logger.debug(f'{yes_votes} for, {no_votes} against')
+
+    now = datetime.datetime.now().timestamp()
+    reminder_delta = now - float(proposal.data.get('reminder_sent'))
+
+    if datetime.timedelta(seconds=int(reminder_delta)) > datetime.timedelta(days=variables.reminder_window_in_days):
+        proposal.data.set('reminder_sent', now)
+
+        already_voted = proposal.get_all_boolean_votes().values_list('user__username')
+        already_voted_list = [voter[0] for voter in already_voted]
+        nonvoter_list = [f'<@{uid}>' for uid in variables.users if uid not in already_voted_list]
+        nonvoter_string = ', '.join(nonvoter_list)
+        voter_msg = f'Eligible voters: {variables.users}; Already voted: {already_voted}; Nonvoter list: {nonvoter_list}'
+        slack.post_message(text=voter_msg, channel=variables.vote_channel, thread_ts=proposal.vote_post_id)
+
+        reminder_msg = f'Please vote on this proposal. {nonvoter_string} have not voted yet.'
+        slack.post_message(text=reminder_msg, channel=variables.vote_channel, thread_ts=proposal.vote_post_id)
+
     if yes_votes >= len(variables.users) / 2:
       return PASSED
     elif no_votes >= len(variables.users) / 2:
@@ -79,14 +92,90 @@ def majority_vote__check():
     return PROPOSED
     
 
+def consensus__check():
+    if not proposal.vote_post_id:
+        return None
+    yes_votes = proposal.get_yes_votes().count()
+    no_votes = proposal.get_no_votes().count()
+    logger.debug(f'{yes_votes} for, {no_votes} against')
+
+    now = datetime.datetime.now().timestamp()
+    reminder_delta = now - float(proposal.data.get('reminder_sent'))
+
+    if datetime.timedelta(seconds=int(reminder_delta)) > datetime.timedelta(days=variables.reminder_window_in_days):
+        proposal.data.set('reminder_sent', now)
+
+        already_voted = proposal.get_all_boolean_votes().values_list('user__username')
+        already_voted_list = [voter[0] for voter in already_voted]
+        nonvoter_list = [f'<@{uid}>' for uid in variables.users if uid not in already_voted_list]
+        nonvoter_string = ', '.join(nonvoter_list)
+        voter_msg = f'Eligible voters: {variables.users}; Already voted: {already_voted}; Nonvoter list: {nonvoter_list}'
+        slack.post_message(text=voter_msg, channel=variables.vote_channel, thread_ts=proposal.vote_post_id)
+
+        reminder_msg = f'Please vote on this proposal. {nonvoter_string} have not voted yet.'
+        slack.post_message(text=reminder_msg, channel=variables.vote_channel, thread_ts=proposal.vote_post_id)
+
+    if no_votes >= 1:
+      return FAILED
+    
+    elif yes_votes >= len(variables.users):
+      return PASSED
+    
+    
+    return PROPOSED
+
+
+def custom_voting__check():
+    if not proposal.vote_post_id:
+        return None
+    yes_votes = proposal.get_yes_votes().count()
+    no_votes = proposal.get_no_votes().count()
+    logger.debug(f'{yes_votes} for, {no_votes} against')
+
+    now = datetime.datetime.now().timestamp()
+    reminder_delta = now - float(proposal.data.get('reminder_sent'))
+
+    if datetime.timedelta(seconds=int(reminder_delta)) > datetime.timedelta(days=variables.reminder_window_in_days):
+        proposal.data.set('reminder_sent', now)
+
+        already_voted = proposal.get_all_boolean_votes().values_list('user__username')
+        already_voted_list = [voter[0] for voter in already_voted]
+        nonvoter_list = [f'<@{uid}>' for uid in variables.users if uid not in already_voted_list]
+        nonvoter_string = ', '.join(nonvoter_list)
+        voter_msg = f'Eligible voters: {variables.users}; Already voted: {already_voted}; Nonvoter list: {nonvoter_list}'
+        slack.post_message(text=voter_msg, channel=variables.vote_channel, thread_ts=proposal.vote_post_id)
+
+        reminder_msg = f'Please vote on this proposal. {nonvoter_string} have not voted yet.'
+        slack.post_message(text=reminder_msg, channel=variables.vote_channel, thread_ts=proposal.vote_post_id)
+
+    if no_votes >= variables.no_votes_to_fail:
+      return FAILED
+
+    elif yes_votes >= variables.yes_votes_to_pass:
+      return PASSED
+    
+    
+    return PROPOSED
+
 def print_for_json(f):
     ret = inspect.getsource(f)
     ret = ret.replace('\n    ', '\\n')
-    ret = ret.split("():")[1]
+    ret = ret.replace('\n', '')
+    ret = ret.replace("'", "\\\"")
+    ret = ret.split('():')[1]
     print(ret)
     return ret
 
 print_for_json(peer_approval__check)
+print()
 
-# print_for_json(majority_vote__check)
+print_for_json(majority_vote__check)
+print()
+
+# print_for_json(consensus__check)
+# print()
+
+# print_for_json(custom_voting__check)
+# print()
+
 
